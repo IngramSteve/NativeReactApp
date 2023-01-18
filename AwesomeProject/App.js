33eba6e52f53279407663d6dadc1cfcbd72a3de8
IngramSteve <steve.brzezinski@ingrammicro.com>
2023-01-18 16:15:35 +0100

    Cat class

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import Cat from "./components/firstSteps/Cat";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Text><Cat /></Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ AwesomeProject/components/firstSteps/Cat.js
@@ -0,0 +1,13 @@
+import {Component} from "react";
+
+class Cat extends Component{
+    render() {
+        return (
+            <div className={"Cat"}>
+                <p>Hallo, ich bin deine Katze!</p>
+            </div>
+        )
+    }
+}
+
+export default Cat;
